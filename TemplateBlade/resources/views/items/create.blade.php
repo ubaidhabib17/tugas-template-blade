9b213784802aeb7d8a24d962d22ca1db71c40cf2
ubaidhabib17 <ubaidhabib17@gmail.com>
2020-07-02 00:26:16 +0700

    sudah bosku

---
+++ TemplateBlade/resources/views/items/create.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte.master')
+
+@section('content')
+    <h2>ini halaman create</h2>
+@endsection
+
+@push('scripts')
+<script>
+    
+</script>
+@endpush
---
+++ TemplateBlade/resources/views/items/index.blade.php
@@ -0,0 +1,8 @@
+@extends('adminlte.master')
+
+@section('content')
+    
+    <h1>Halo gaes</h1>
+
+
+@endsection
